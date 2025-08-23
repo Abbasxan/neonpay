43ba3a7a3d9aabb4898be94a7174b03f9018306c
Abbasxan <138109623+Abbasxan@users.noreply.github.com>
2025-08-23 17:02:28 +0400

    Update pyrogram_bot.py

--- examples/pyrogram_bot.py
+++ examples/pyrogram_bot.py
@@ -1,19 +1,15 @@
 from pyrogram import Client, filters
-from neon_stars_payments import NeonStars, StarsPaymentError
+from neonpay import NeonStars, StarsPaymentError
 
 app = Client("stars_bot", api_id=12345, api_hash="your_api_hash", bot_token="YOUR_BOT_TOKEN")
 
-# Инициализация библиотеки
 stars = NeonStars(app, thank_you="Спасибо за поддержку!")
 
-# Обработка успешной оплаты
 @stars.on_payment
 async def handle_payment(user_id: int, amount: int):
     print(f"🎉 Пользователь {user_id} оплатил {amount} ⭐")
-    # Здесь можно сохранять в БД или выдавать плюшки
 
-
-@app.on_message(filters.command("donate") & filters.private)
+@app.on_message(filters.command("donate"))
 async def donate(client, message):
     try:
         await stars.send_donate(
@@ -21,10 +17,9 @@ async def donate(client, message):
             amount=50,
             label="🥐 50 ⭐",
             title="Поддержка проекта",
-            description="Спасибо за твою поддержку!",
+            description="Спасибо за твою поддержку!"
         )
     except StarsPaymentError as e:
         await message.reply(f"Ошибка: {e}")
-
 
 app.run()
